0626c951ead8f0b8123b3e971cebdb94f120514f
alkadoHs <alkadosichone@gmail.com>
2023-11-22 00:21:41 +0300

    restore stock after deleting order items

--- app/Http/Controllers/OrderItemController.php
+++ app/Http/Controllers/OrderItemController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Models\OrderItem;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class OrderItemController extends Controller
 {
     public function destroy(OrderItem $orderItem)
     {
+        $product = Product::find($orderItem->product_id);
+        // restore the products stock
+        $product->increment("stock", $orderItem->quantity);
+
         $orderItem->delete();
         return back();
     }
