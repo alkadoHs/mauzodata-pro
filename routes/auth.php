e56051d3096a265c2c42c884360d45c3ced5e075
alkadoHs <alkadosichone@gmail.com>
2024-04-16 13:21:00 +0300

    register auth

--- routes/auth.php
+++ routes/auth.php
@@ -11,9 +11,9 @@ use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth')->group(function () {
+Route::middleware('guest')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])
-                ->name('register');
+                ->name('register')->middleware(['auth']);
 
     Route::post('register', [RegisteredUserController::class, 'store']);
 
