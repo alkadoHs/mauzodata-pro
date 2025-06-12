68766ef40576622a4a22c56b941a11fc04507353
alkadoHs <alkadosichone@gmail.com>
2025-06-12 06:36:58 +0300

    supplier modeling

--- app/Models/Supplier.php
+++ app/Models/Supplier.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Supplier extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable = ['company_id', 'name', 'email', 'phone', 'address'];
+
+    public function purchaseOrders(): HasMany
+    {
+        return $this->hasMany(PurchaseOrder::class);
+    }
 }
