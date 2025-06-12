2cd320c8e2bc3aa378e55e352c8dc3e0c61febde
alkadoHs <alkadosichone@gmail.com>
2025-06-12 06:37:25 +0300

    purchase order modeling

--- app/Models/PurchaseOrder.php
+++ app/Models/PurchaseOrder.php
@@ -2,9 +2,33 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class PurchaseOrder extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable = ['branch_id', 'user_id', 'supplier_id', 'status', 'notes'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function branch(): BelongsTo
+    {
+        return $this->belongsTo(Branch::class);
+    }
+
+    public function supplier(): BelongsTo
+    {
+        return $this->belongsTo(Supplier::class);
+    }
+
+    public function items(): HasMany
+    {
+        return $this->hasMany(PurchaseOrderItem::class);
+    }
 }
